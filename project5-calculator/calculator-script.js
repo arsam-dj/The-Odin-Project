3789ad6f1677319e628618749e6c8dbddf78ff9e
InfinityCent <pereeia@gmail.com>
2024-09-20 18:06:23 -0400

    Improved aesthetics, added two additional operators, and finalized a functioning calculator.

--- project5-calculator/calculator-script.js
+++ project5-calculator/calculator-script.js
@@ -1,15 +1,84 @@
+// ------------------- Miscellaneous ------------------- //
+const operators = "+−×÷√^."
+
+
 // ------------------- Display ------------------- //
 let displayPara = document.querySelector('#displayText')
 
 function changeDisplay(text) {
     currDisplay = displayPara.innerText
-    if (currDisplay !== '') {
+    displayClass = displayPara.className
+    if ((currDisplay !== '') & (displayClass === 'basic')) {
         text = currDisplay + text
     }
 
+    displayPara.className = 'basic'
     displayPara.innerText = text
     displayPara.style.padding = '0px' 
 }
+
+
+// ------------------- Buttons ------------------- //
+const numberButtons = document.querySelectorAll('.numpad')
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', (event) =>
+    changeDisplay(event.target.textContent)
+)
+})
+
+const operatorButtons = document.querySelectorAll('.operator')
+operatorButtons.forEach(operatorButton => {
+    if (operatorButton.id === 'sqrt') {
+        operatorButton.addEventListener('click', (event) => {
+            changeDisplay(event.target.textContent)
+        })
+    } else {
+        operatorButton.addEventListener('click', (event) => {
+            if (displayPara.className === 'output') {
+                displayPara.className = 'basic'
+            }
+            changeDisplay(event.target.textContent)
+        })
+    }
+})
+
+const plusminusButton = document.querySelector('#plusminus')
+plusminusButton.addEventListener('click', () => {
+    equation = displayPara.innerText
+    splitEquation = equation.split(/(\+|\−|\×|\÷|\√|\^)/g)
+    if (splitEquation[1] === undefined) {
+        if (equation[0] === '-') {
+            displayPara.innerText = equation.slice(1)
+        } else {
+            displayPara.innerText = '-'+ displayPara.innerText
+        }
+        
+    } else {
+        secondNum = splitEquation[2]
+        if (secondNum[0] === '-') {
+            secondNum = secondNum.slice(1)
+        } else {
+            secondNum = '-' + secondNum
+        }
+        splitEquation[2] = secondNum
+        displayPara.innerText = splitEquation.join('')
+    }
+    
+})
+
+const clearButton = document.querySelector('#clear')
+clearButton.addEventListener('click', () => {
+    displayPara.innerText = ''
+    displayPara.className = 'basic'
+})
+
+const delButton = document.querySelector('#backspace')
+delButton.addEventListener('click', () => {
+    if (displayPara.className === 'basic') {
+        displayPara.innerText = displayPara.innerText.slice(0,-1)
+    }
+})
+
 
 
 // ------------- Operator Functions ------------- //
@@ -30,51 +99,70 @@ function divide(...params) {
     return(params.reduce((paramDiv, a) => paramDiv / a))
 }
 
-function operate(firstNum, operator, secondNum) {
+function sqrt(a, b=2) {
+    return(a**(1/b))
+}
 
+function exp(a, b) {
+    return(a ** b)
+}
+
+function operate(firstNum, operator, secondNum=2) {
+    let result = 0
     switch(operator) {
         case '+':
-            return add(+firstNum, +secondNum);
-        
-        case '-':
-            return subtract(+firstNum, +secondNum);
-        
-        case '*':
-            return multiply(+firstNum, +secondNum);
-
-        case '/':
-            if (secondNum == 0) {
-                return 'smartass';
-            } else {
-                return divide(+firstNum, +secondNum);
+            result = add(+firstNum, +secondNum)
+            break;
+        case '−':
+            result = subtract(+firstNum, +secondNum);
+            break;
+        case '×':
+            result =  multiply(+firstNum, +secondNum);
+            break;
+        case '÷':
+            if (+secondNum === 0) {
+                return 'nice try...'
             }
+            result = divide(+firstNum, +secondNum);
+            break;
+        case '√':
+            result = sqrt(+firstNum, +secondNum)
+            break;
+        case '^':
+            result = exp(+firstNum, +secondNum)
+            break;
+    }
+    if (Number.isInteger(result)) {
+        return result
+    } else {
+        return Number.parseFloat(result.toFixed(8))
     }
 }
 
-// ------------------- Buttons ------------------- //
-const displayButtons = document.querySelectorAll('.numpad, .operator')
-displayButtons.forEach(displayButton => {
-    displayButton.addEventListener('click', (event) =>
-    changeDisplay(event.target.textContent)
-)
+
+
+// ------------- Operator ------------- //
+const equalButton = document.querySelector('#equal')
+
+equalButton.addEventListener('click', () => {
+    let equation = displayPara.innerText
+    if (operators.includes(equation.slice(-1))) {
+        displayPara.innerText = 'SEMANTIC ERROR'
+    } else {
+        splitEquation = equation.split(/(\+|\−|\×|\÷|\√|\^)/g)
+
+        let firstNum = splitEquation[0]
+        let operator = splitEquation[1]
+        let secondNum = splitEquation[2]
+
+        if (operator === '√') {
+            firstNum = splitEquation[2]
+            secondNum = 2
+        }
+        
+        if (!(operator === undefined)) {
+            displayPara.innerText = operate(firstNum, operator, secondNum)
+        }
+    }
+    displayPara.className = 'output'
 })
-
-const clearButton = document.querySelector('#clear')
-clearButton.addEventListener('click', () => {
-    displayPara.innerText = ''
-    displayPara.style.height = '20px'
-    displayPara.style.width = '120px'
-})
-
-const delButton = document.querySelector('#backspace')
-delButton.addEventListener('click', () => {
-    displayPara.innerText = displayPara.innerText.slice(0,-1)
-    displayPara.style.height = '20px'
-    displayPara.style.width = '120px'
-})
-
-/* --------------------------------------------------- */
-
-let firstNum
-let operator
-let secondNum
