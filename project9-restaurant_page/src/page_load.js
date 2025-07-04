af026d06613f692e70452d78b97590d175b8dd89
arsam-dj <pereeia@gmail.com>
2025-07-04 17:53:40 -0400

    Started putting together the menu page.

--- project9-restaurant_page/src/page_load.js
+++ project9-restaurant_page/src/page_load.js
@@ -1,5 +1,9 @@
 import cafeImage from "./images/cafe_image.jpg";
-import contactImage from "/home/alex/The-Odin-Project/project9-restaurant_page/src/images/beach_phone.jpg";
+import contactImage from "./images/beach_phone.jpg";
+import coffeeImage from "./images/coffee_espresso.jpg";
+import iceCreamImage from "./images/strawberry_icecream.jpg";
+import lemonadeImage from "./images/lemonade_soda.jpg";
+import bananaBreadImage from "./images/banana_bread.jpg";
 
 function create_header() {
     
@@ -12,7 +16,7 @@ function create_header() {
     homeBtn.id = 'home-btn'
     
     const menuBtn = document.createElement('button')
-    menuBtn.addEventListener('click', () => {console.log("hi")})
+    menuBtn.addEventListener('click', create_menu)
     menuBtn.id = 'menu-btn'
     
     const contactBtn = document.createElement('button')
@@ -73,6 +77,110 @@ function create_homepage() {
 }
 
 
+function create_menu() {
+    
+    /* -------------------- CREATING MENU PAGE -------------------- */
+    // Remove any child nodes except for header
+    while (document.body.childNodes.length > 2) {
+    document.body.removeChild(document.body.lastChild);
+    }
+    
+    // Create a menu div below header and create different food-type subdivs within it
+    const menu = document.createElement('div')
+    const coffeeDiv = document.createElement('div')
+    const iceCreamDiv = document.createElement('div')
+    const drinksDiv = document.createElement('div')
+    const bakedGoodsDiv = document.createElement('div')
+    
+    menu.id = 'menu'
+    coffeeDiv.id = 'coffee-div'
+    iceCreamDiv.id = 'icecream-div'
+    drinksDiv.id = 'drinks-div'
+    bakedGoodsDiv.id = 'baked-goods-div'
+
+    // Add images and foods to each menu div
+
+    // Espresso and Coffee
+    const image1 = document.createElement("img");
+    image1.src = coffeeImage;
+    image1.className = 'menu-img'
+
+    const coffeeMenu = document.createElement('div')
+    coffeeMenu.className = 'menu-subdiv'
+
+    const coffeeDivHeader = document.createElement('p')
+    coffeeDivHeader.className = 'menuDivHeader'
+    coffeeDivHeader.textContent = 'Coffee & Espresso'
+
+    const coffee1 = document.createElement('div')
+    coffee1.className = 'item-subdiv'
+    const coffee1Header = document.createElement('p')
+    coffee1Header.textContent = 'Seaside Brew'
+    const coffee1Body = document.createElement('p')
+    coffee1Body.textContent = 'Freshly ground medium roast, brewed to perfection using locally sourced beans'
+    const coffee1Ingredients = document.createElement('p')
+    coffee1Ingredients.textContent = 'Ingredients: Arabica coffee beans, filtered water'
+    coffee1.append(coffee1Header, coffee1Body, coffee1Ingredients)
+
+    const coffee2 = document.createElement('div')
+    coffee2.className = 'item-subdiv'
+    const coffee2Header = document.createElement('p')
+    coffee2Header.textContent = 'Salted Caramel Latte'
+    const coffee2Body = document.createElement('p')
+    coffee2Body.textContent = 'Espresso with steamed milk, homemade salted caramel sauce, topped with sea salt flakes'
+    const coffee2Ingredients = document.createElement('p')
+    coffee2Ingredients.textContent = 'Ingredients: Espresso, whole milk, caramel (sugar, butter, cream), sea salt'
+    coffee2.append(coffee2Header, coffee2Body, coffee2Ingredients)
+
+    const coffee3 = document.createElement('div')
+    coffee3.className = 'item-subdiv'
+    const coffee3Header = document.createElement('p')
+    coffee3Header.textContent = 'Vanilla Coconut Cold Brew'
+    const coffee3Body = document.createElement('p')
+    coffee3Body.textContent = 'Smooth cold brew coffee infused with natural vanilla and coconut milk'
+    const coffee3Ingredients = document.createElement('p')
+    coffee3Ingredients.textContent = 'Ingredients: Cold brew concentrate, vanilla extract, coconut milk, ice'
+    coffee3.append(coffee3Header, coffee3Body, coffee3Ingredients)
+
+    const coffee4 = document.createElement('div')
+    coffee4.className = 'item-subdiv'
+    const coffee4Header = document.createElement('p')
+    coffee4Header.textContent = 'Cinnamon Drift Cappuccino'
+    const coffee4Body = document.createElement('p')
+    coffee4Body.textContent = 'Espresso topped with steamed milk and a sprinkle of cinnamon dust'
+    const coffee4Ingredients = document.createElement('p')
+    coffee4Ingredients.textContent = 'Ingredients: Espresso, whole milk, cinnamon powder'
+    coffee4.append(coffee4Header, coffee4Body, coffee4Ingredients)
+
+    coffeeMenu.append(coffeeDivHeader, coffee1, coffee2, coffee3, coffee4)
+    coffeeDiv.append(image1, coffeeMenu)
+    
+    
+    // Ice Cream Scoops
+    const image2 = document.createElement("img");
+    image2.src = iceCreamImage;
+    image2.className = 'menu-img'
+    iceCreamDiv.appendChild(image2)
+
+    // Other Drinks
+    const image3 = document.createElement("img");
+    image3.src = lemonadeImage;
+    image3.className = 'menu-img'
+    drinksDiv.appendChild(image3)
+
+    // Baked Goods
+    const image4 = document.createElement("img");
+    image4.src = bananaBreadImage;
+    image4.className = 'menu-img'
+    bakedGoodsDiv.appendChild(image4)
+    
+    // Append content to document body
+    menu.append(coffeeDiv, iceCreamDiv, drinksDiv, bakedGoodsDiv)
+    document.body.appendChild(menu)
+
+}
+
+
 function create_contact() {
     
     /* -------------------- CREATING CONTACT PAGE -------------------- */
@@ -113,16 +221,24 @@ function create_contact() {
 
     const contactHours = document.createElement('div')
     contactHours.id = 'contact-hours'
+
+    const hoursHeader = document.createElement('p')
+    hoursHeader.id = 'hours-header'
+    hoursHeader.textContent = "Hours of Operation"
+
     const montues = document.createElement('p')
+    montues.id = 'hours'
     montues.textContent = "Monday, Tuesday: CLOSED"
 
     const wedthurs = document.createElement('p')
+    wedthurs.id = 'hours'
     wedthurs.textContent = "Wednesday, Thursday: 8AM - 8PM"
 
     const frisatsun = document.createElement('p')
+    frisatsun.id = 'hours'
     frisatsun.textContent = "Friday, Saturday, Sunday: 8AM - 11PM"
 
-    contactHours.append(montues, wedthurs, frisatsun)
+    contactHours.append(hoursHeader, montues, wedthurs, frisatsun)
     
     contactInfo.append(contactHeader, contactPhone, contactAddress, contactHours)
     
@@ -136,12 +252,5 @@ function load_homepage() {
     create_homepage()
 }
 
-
-function add_button_functionality() {
-    // Home Button
-    homeBtn = document.getElementById('home-btn')
-    
-    
-}
-export {load_homepage, add_button_functionality}
+export {load_homepage}
 
