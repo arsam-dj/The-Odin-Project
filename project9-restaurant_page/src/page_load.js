a33fd203cc855b3d9bf84d0755e2f980d886e97d
arsam-dj <pereeia@gmail.com>
2025-07-08 16:17:16 -0400

    Added more menu items.

--- project9-restaurant_page/src/page_load.js
+++ project9-restaurant_page/src/page_load.js
@@ -160,19 +160,126 @@ function create_menu() {
     const image2 = document.createElement("img");
     image2.src = iceCreamImage;
     image2.className = 'menu-img'
-    iceCreamDiv.appendChild(image2)
+
+    const iceCreamMenu = document.createElement('div')
+    iceCreamMenu.className = 'menu-subdiv'
+
+    const iceCreamDivHeader = document.createElement('p')
+    iceCreamDivHeader.className = 'menuDivHeader'
+    iceCreamDivHeader.textContent = 'Ice Cream Scoops'
+
+    const iceCream1 = document.createElement('div')
+    iceCream1.className = 'item-subdiv'
+    const iceCream1Header = document.createElement('p')
+    iceCream1Header.textContent = 'Coastal Cream Classic'
+    const iceCream1Body = document.createElement('p')
+    iceCream1Body.textContent = 'Rich vanilla bean ice cream made in-house with real Madagascar vanilla'
+    const iceCream1Ingredients = document.createElement('p')
+    iceCream1Ingredients.textContent = 'Ingredients: Milk, cream, sugar, vanilla bean'
+    iceCream1.append(iceCream1Header, iceCream1Body, iceCream1Ingredients)
+
+    const iceCream2 = document.createElement('div')
+    iceCream2.className = 'item-subdiv'
+    const iceCream2Header = document.createElement('p')
+    iceCream2Header.textContent = 'Sea Breeze Strawberry'
+    const iceCream2Body = document.createElement('p')
+    iceCream2Body.textContent = 'Fresh strawberry ice cream with chunks of local strawberries'
+    const iceCream2Ingredients = document.createElement('p')
+    iceCream2Ingredients.textContent = 'Ingredients: Milk, cream, sugar, fresh strawberries'
+    iceCream2.append(iceCream2Header, iceCream2Body, iceCream2Ingredients)
+
+    const iceCream3 = document.createElement('div')
+    iceCream3.className = 'item-subdiv'
+    const iceCream3Header = document.createElement('p')
+    iceCream3Header.textContent = 'Chocolate Wave'
+    const iceCream3Body = document.createElement('p')
+    iceCream3Body.textContent = 'Decadent dark chocolate ice cream with cocoa nibs'
+    const iceCream3Ingredients = document.createElement('p')
+    iceCream3Ingredients.textContent = 'Ingredients: Milk, cream, sugar, dark cocoa powder, cocoa nibs'
+    iceCream3.append(iceCream3Header, iceCream3Body, iceCream3Ingredients)
+
+    iceCreamMenu.append(iceCreamDivHeader, iceCream1, iceCream2, iceCream3)
+    iceCreamDiv.append(image2, iceCreamMenu)
 
     // Other Drinks
     const image3 = document.createElement("img");
     image3.src = lemonadeImage;
     image3.className = 'menu-img'
-    drinksDiv.appendChild(image3)
+
+    const drinksMenu = document.createElement('div')
+    drinksMenu.className = 'menu-subdiv'
+
+    const drinksDivHeader = document.createElement('p')
+    drinksDivHeader.className = 'menuDivHeader'
+    drinksDivHeader.textContent = 'Refreshing Drinks'
+
+    const drinks1 = document.createElement('div')
+    drinks1.className = 'item-subdiv'
+    const drinks1Header = document.createElement('p')
+    drinks1Header.textContent = 'Ocean Mist Lemonade Soda'
+    const drinks1Body = document.createElement('p')
+    drinks1Body.textContent = 'Homemade lemonade mixed with sparkling water and a hint of mint'
+    const drinks1Ingredients = document.createElement('p')
+    drinks1Ingredients.textContent = 'Ingredients: Fresh lemon juice, sparkling water, sugar, fresh mint leaves'
+    drinks1.append(drinks1Header, drinks1Body, drinks1Ingredients)
+
+    const drinks2 = document.createElement('div')
+    drinks2.className = 'item-subdiv'
+    const drinks2Header = document.createElement('p')
+    drinks2Header.textContent = 'Ginger Tidal Fizz'
+    const drinks2Body = document.createElement('p')
+    drinks2Body.textContent = 'Ginger syrup with club soda and fresh lime'
+    const drinks2Ingredients = document.createElement('p')
+    drinks2Ingredients.textContent = 'Ingredients: Ginger root syrup, club soda, lime juice'
+    drinks2.append(drinks2Header, drinks2Body, drinks2Ingredients)
+
+    drinksMenu.append(drinksDivHeader, drinks1, drinks2)
+    drinksDiv.append(image3, drinksMenu)
 
     // Baked Goods
     const image4 = document.createElement("img");
     image4.src = bananaBreadImage;
     image4.className = 'menu-img'
-    bakedGoodsDiv.appendChild(image4)
+
+    const bakedGoodsMenu = document.createElement('div')
+    bakedGoodsMenu.className = 'menu-subdiv'
+
+    const bakedGoodsDivHeader = document.createElement('p')
+    bakedGoodsDivHeader.className = 'menuDivHeader'
+    bakedGoodsDivHeader.textContent = 'Homemade Baked Goods'
+
+    const bakedGoods1 = document.createElement('div')
+    bakedGoods1.className = 'item-subdiv'
+    const bakedGoods1Header = document.createElement('p')
+    bakedGoods1Header.textContent = 'Driftwood Blueberry Muffin'
+    const bakedGoods1Body = document.createElement('p')
+    bakedGoods1Body.textContent = 'Moist muffin packed with fresh blueberries and a hint of lemon zest'
+    const bakedGoods1Ingredients = document.createElement('p')
+    bakedGoods1Ingredients.textContent = 'Ingredients: Flour, sugar, eggs, butter, fresh blueberries, lemon zest, baking powder'
+    bakedGoods1.append(bakedGoods1Header, bakedGoods1Body, bakedGoods1Ingredients)
+
+    const bakedGoods2 = document.createElement('div')
+    bakedGoods2.className = 'item-subdiv'
+    const bakedGoods2Header = document.createElement('p')
+    bakedGoods2Header.textContent = 'Salt Spray Scone'
+    const bakedGoods2Body = document.createElement('p')
+    bakedGoods2Body.textContent = 'Flaky scone with sea salt flakes and cheddar cheese'
+    const bakedGoods2Ingredients = document.createElement('p')
+    bakedGoods2Ingredients.textContent = 'Ingredients: Flour, butter, cheddar cheese, sea salt, baking powder'
+    bakedGoods2.append(bakedGoods2Header, bakedGoods2Body, bakedGoods2Ingredients)
+
+    const bakedGoods3 = document.createElement('div')
+    bakedGoods3.className = 'item-subdiv'
+    const bakedGoods3Header = document.createElement('p')
+    bakedGoods3Header.textContent = 'Beachcomber Banana Bread'
+    const bakedGoods3Body = document.createElement('p')
+    bakedGoods3Body.textContent = 'Sweet banana bread with pecans and cinnamon'
+    const bakedGoods3Ingredients = document.createElement('p')
+    bakedGoods3Ingredients.textContent = 'Ingredients: Ripe bananas, flour, sugar, eggs, pecans, cinnamon, baking soda'
+    bakedGoods3.append(bakedGoods3Header, bakedGoods3Body, bakedGoods3Ingredients)
+
+    bakedGoodsMenu.append(bakedGoodsDivHeader, bakedGoods1, bakedGoods2, bakedGoods3)
+    bakedGoodsDiv.append(image4, bakedGoodsMenu)
     
     // Append content to document body
     menu.append(coffeeDiv, iceCreamDiv, drinksDiv, bakedGoodsDiv)
